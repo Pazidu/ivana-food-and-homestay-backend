98fde8343e2de87ae746cd73e4950493b6026bc1
Pasindu Wilagama <pasidubhagya20@gmail.com>
2026-03-15 09:02:30 +0530

    refactor: update service account import to use createRequire

--- Firebase/admin.js
+++ Firebase/admin.js
@@ -1,6 +1,9 @@
 // Firebase/admin.js
 import admin from "firebase-admin";
-import serviceAccount from "../contra-cloud-firebase-adminsdk-ow8ha-7792acf400.json" assert { type: "json" };
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const serviceAccount = require("../contra-cloud-firebase-adminsdk-ow8ha-7792acf400.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
